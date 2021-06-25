bc6e6e72de0b34f0f8c2c6b330ae35015bd75984
Riebot <86209111+Riebot@users.noreply.github.com>
2021-06-25 14:45:58 +0700

    Update song.py

--- MightyMusic/modules/song.py
+++ MightyMusic/modules/song.py
@@ -58,7 +58,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**Berhasil Di Upload 🎵\n◎› Info: @infobotmusik \n◎› Owner Project: [arie](https://t.me/SilenceSpe4ks) **"
+        rep = "**Berhasil Di Upload 🎶\n🎧› Info: @infobotmusik \n☕› Owner Project: [arie](https://t.me/SilenceSpe4ks) **"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
