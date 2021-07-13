0ae1b1fba4cf5632de24780f6467a747b611b9f0
Riebot <86209111+Riebot@users.noreply.github.com>
2021-07-13 21:08:14 +0700

    Create song.py

--- MightyMusic/modules/song.py
+++ MightyMusic/modules/song.py
@@ -32,7 +32,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    m = message.reply("🔎 Mencari lagu tersebut...")
+    m = message.reply("🔎 Sedang Mencari lagu tersebut...")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -52,13 +52,13 @@ def song(client, message):
         m.edit("❌ Tidak ditemukan.\n\nCoba cari lagu lain atau eja dengan benar.")
         print(str(e))
         return
-    m.edit("Mendowload lagu tersebut ")
+    m.edit("📥Sedang Mendowload lagu tersebut🔄..Sabar Yaa ")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**Berhasil Di Upload 🎶\n🎧› Info: @infobotmusik \n☕› Owner Project: [arie](https://t.me/SilenceSpe4ks) **"
+        rep = "**Berhasil Di Upload Di Sini🎶\n🎧› Channel: @infobotmusik \n☕› Owner Project: [arie](https://t.me/SilenceSpe4ks) **"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
